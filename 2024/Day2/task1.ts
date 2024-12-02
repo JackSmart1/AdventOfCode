57f1f41e94dabaafab20a84c95bb981e4acd58aa
Jack Smart <Jacksmart52@gmail.com>
2024-12-02 07:49:52 +0000

    Day 1

---
+++ 2024/Day2/task1.ts
@@ -0,0 +1,26 @@
+let input = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+let redNoseReports = input => {
+
+    let result = 0;
+
+    let reports = input.split('\n');
+    reports
+    reports.forEach(r => checkLevel(r))
+}
+
+const checkLevel = report => {
+    const maxDif = 3;
+    report
+    report.split(' ').forEach((element, i) => {
+
+    });
+}
+
+
+redNoseReports(input)
